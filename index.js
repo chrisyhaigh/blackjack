dfc444b20605e25c67cb338d12e53838bd2fb556
Chrisy <chrisyhaigh@gmail.com>
2023-12-11 22:52:14 +0000

    fixed random card function first & second card should only display on start game

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+
+let cards = [];
+let sum = 0;
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
@@ -11,10 +10,26 @@ let cardsEl = document.getElementById("cards-el")
 
 
 function getRandomCard() {
-    return Math.floor( Math.random()*11 ) + 1
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+
+    if (randomNumber == 1) {
+        return 11
+    } else if (randomNumber > 10) {
+        return 10
+    } else {
+        return randomNumber
+    }
+
 }
 
 function startGame() {
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+
+    cards = [firstCard, secondCard]
+
+    sum = firstCard + secondCard
     renderGame()
 }
 
@@ -42,7 +57,7 @@ function renderGame() {
 
 function newCard() {
 
-    if (isAlive && sum <= 21) {
+    if (isAlive && !hasBlackJack) {
         let card = getRandomCard()
         sum += card
         cards.push(card)
@@ -54,8 +69,8 @@ function newCard() {
     messageEl.textContent = message;
 }
 
-function resetCards() {
+/*function resetCards() {
     messageEl.textContent = 'Want to play a round?';
     cardsEl.textContent = 'Cards: ';
     sumEl.textContent = 'Sum: ';
-}
+}*/
